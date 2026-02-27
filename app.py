87e652e450b38bb49d44619d664f3f9b8c22af6b
mangeshmohrle90 <mangeshmoharle90@gmail.com>
2026-02-27 03:46:19 +0000

    test1

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 
-
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-print(a+b)
+    return "Container running successfully"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
