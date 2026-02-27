a8aeaff9c3cbfee3d5b1e6785e755397a7fa9a15
mangeshmohrle90 <mangeshmoharle90@gmail.com>
2026-02-27 03:42:09 +0000

    test

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps CI/CD Working Successfully , file changed again!!"
+    return 
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a+b)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
